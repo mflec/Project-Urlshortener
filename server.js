9aecbafb54ab98648508ee7bd1a21c92f48157b9
Glitch (laced-disco-jute) <none>
2021-06-29 23:54:00 +0000

    🐽💶 Checkpoint ./package.json:33368020/1820 ./server.js:33368020/1030

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mongoose= require("mongoose")
+const { Schema } = mongoose;
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const schema= new Schema({url:"string"})
+const Url= mongoose.model('Url', schema)
+
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
