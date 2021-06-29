219d7dbe1dbd0b64be61d2751325d511838e700c
Glitch (laced-disco-jute) <none>
2021-06-29 23:03:01 +0000

    🚀🌜 Checkpoint ./server.js:33368020/45546 ./package.json:33368020/1594

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-require('dotenv').config();
+'use strict';
+
 const express = require('express');
+// var mongo = require('mongodb');
+const mongoose = require('mongoose');
+
 const cors = require('cors');
+
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+/** this project needs a db !! **/
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+/** this project needs to parse POST bodies **/
+// you should mount the body-parser here
+app.use(express.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
+const healthcheckRoutes = require('./domains/healthcheck/routes');
+app.use('/api', healthcheckRoutes);
+
+const shortUrlRoutes = require('./domains/short-url/routes');
+app.use('/api/shorturl', shortUrlRoutes);
+
+app.listen(port, function () {
+  console.log('Node.js listening ...');
 });
