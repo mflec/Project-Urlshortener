6d265dfc68574d3261e39035bf8fd6731308e17b
Glitch (laced-disco-jute) <none>
2021-06-29 23:26:01 +0000

    🚝🎋 Checkpoint ./server.js:33368020/30532 ./sample.env:33368020/361 ./package.json:33368020/8564

--- server.js
+++ server.js
@@ -1,68 +1,24 @@
-'use strict';
-var dns = require('dns')
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser')
-var cors = require('cors');
-require('dotenv')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-var app = express();
-
-var port = process.env.PORT || 3000;
-
-process.env.MONGO_URI="mongodb+srv://mflec:40644938m@cluster0.mawps.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
-var connection = mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useFindAndModify: true, useUnifiedTopology: true})
-
-
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "Conection error"))
-db.once("open", console.log.bind(console, "Connected"))
-
-const ShortsSchema = mongoose.Schema({
-  origin: {type: String, required: true},
-  new: {type: Number, required: true, unique: true, default: 0}
-})
-
-const Shorts = mongoose.model("Shorts", ShortsSchema)
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
+app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
+app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-  
-app.post("/api/shorturl/new", (req, res) => {
-  const url = req.body.url.split("/").slice(2,).join("/")
-  dns.lookup(url, (err, adresses, family) => {
-    if (err) res.send({error: "Invalid URL"})
-    else {
-      Shorts.estimatedDocumentCount((err, count) => {
-        if (err) res.send({error: "Failed to fetch number of documents from the database"});
-        var short = new Shorts({origin: req.body.url, new: count + 1})
-        short.save((err, data) => {
-          if (err) res.send({error: "Failed to save URL"})
-          else res.send({url: data.origin, new: data.new})
-        })
-      })
-    }
-  })
-})
 
-app.get("/api/shorturl/:index", (req, res) => {
-  if (!+req.params.index) res.redirect("/")
-  else {
-    Shorts.findOne({new: req.params.index}, (err, data) => {
-      err ? res.send({error: "invalid URL"})
-      : res.redirect(data.origin)
-    })
-  }
-})
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
-app.listen(port, function () {
-  console.log('Node.js listening ...');
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
