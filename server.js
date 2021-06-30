78080364ded2b76aa0a84070ec8d056ab4f1995c
Glitch (laced-disco-jute) <none>
2021-06-30 00:05:12 +0000

    💓📮 Checkpoint ./server.js:33368020/769

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser= require("body-parser")
 const mongoose= require("mongoose")
 const { Schema } = mongoose;
 
@@ -12,7 +13,7 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
 const schema= new Schema({url:"string"})
 const Url= mongoose.model('Url', schema)
 
-
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
@@ -22,6 +23,20 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
+
+app.post('/apo/shorturl/new', async function(req, res) {
+try {
+
+await Url.create({url: "landon"}, (err, data)=>{
+res.json({created: true})
+})
+res.json({ url: req.body.url });
+
+} catch(e) { 
+console.log(e)
+}
+});
+
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
